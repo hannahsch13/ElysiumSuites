5063cafe86518f3ab661f5ffc817d704a0c36c1a
Nicole Casteel <thepronicole888@gmail.com>
2023-09-26 08:19:36 -0500

    click event

--- index.js
+++ index.js
@@ -9,16 +9,33 @@ fetch(url)
 
 //Elements
 const roomMenu = document.querySelector('#rooms-menu')
-console.log(roomMenu)
+// console.log(roomMenu)
+const detailImage = document.querySelector('.detail-image')
+// console.log(detailImage)
+const roomName = document.querySelector('.name')
+// console.log(roomName)
+const roomDescription = document.querySelector('.description')
+// console.log(roomDescription)
+const roomPrice = document.querySelector('.price')
+// console.log(roomPrice)
+const roomLikes = document.querySelector(".likes")
+// console.log(roomLikes)
+
 
 
 function renderImage(arrRooms){
     arrRooms.forEach(roomObj => {
-        console.log(roomObj)
+        // console.log(roomObj)
     const imgMenu = document.createElement("img")
     imgMenu.src = roomObj.image
-    // imgMenu.addEventListener("click", ()=>
-    // )
+    imgMenu.addEventListener("click", ()=>{
+        console.log('click')
+        detailImage.src = roomObj.image
+         roomName.textContent = roomObj.name
+         roomDescription.textContent = "insert"
+         roomPrice.textContent = roomObj.price
+         roomLikes.textContent = `${roomObj.likes} likes `
+    })
 
     roomMenu.append(imgMenu)
     }) 
