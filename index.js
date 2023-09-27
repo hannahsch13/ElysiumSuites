cc0acd5bb41a87d8911be8244c9bcbb9125782c0
Nicole Casteel <thepronicole888@gmail.com>
2023-09-27 10:33:19 -0500

    like button

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //URLS
 url =  "http://localhost:3000/hotelRooms"
-
+// const id = url/
+// console.log(id)
 //Elements
 const roomMenu = document.querySelector('#rooms-menu')
 // console.log(roomMenu)
 const detailImage = document.querySelector('.detail-image')
-console.log(detailImage)
+// console.log(detailImage)
 const roomName = document.querySelector('#name')
 // console.log(roomName)
 const roomDescription = document.querySelector('#description')
@@ -21,36 +22,67 @@ const form = document.getElementById("review-form")
 
 const button = document.getElementById("like-bttn")
 
+
+
 fetch(url)
 .then(resp=> resp.json())
-.then(arrRooms => {
-    arrRooms.forEach(roomObj => {
-        // console.log(roomObj)
-        const imgMenu = document.createElement("img")
-        imgMenu.src = roomObj.image
-        imgMenu.addEventListener("click", ()=>{
-            // console.log('click')
-            detailImage.src = roomObj.image
-            roomName.textContent = roomObj.name
-            roomDescription.textContent = roomObj.description
-            roomPrice.textContent = roomObj.price
-            roomLikes.textContent = `${roomObj.likes} likes`
-            // reviewDisplay.textContent = roomObj.reviews
-            // console.log(roomObj.reviews)
-            reviewDisplay.innerHTML = ""
-            roomObj.reviews.forEach(review => {
-                const reviewLi = document.createElement('li') 
-                //     console.log(review)   
-                reviewLi.textContent = review
-                reviewDisplay.append(reviewLi)
-            })
+.then(arrRooms => arrRooms.forEach(renderRoom))
+    
+function renderRoom(roomObj){
+    // console.log(roomObj)
+    const imgMenu = document.createElement("img")
+    imgMenu.src = roomObj.image
+    imgMenu.addEventListener("click", ()=>{
+        // console.log('click')
+        button.dataset.roomId = roomObj.id
+        detailImage.src = roomObj.image
+        roomName.textContent = roomObj.name
+        roomDescription.textContent = roomObj.description
+        roomPrice.textContent = roomObj.price
+        roomLikes.textContent = `${roomObj.likes} likes`
+        reviewDisplay.innerHTML = ""
+        roomObj.reviews.forEach(review => {
+            const reviewLi = document.createElement('li') 
+            //     console.log(review)   
+            reviewLi.textContent = review
+            reviewDisplay.append(reviewLi)
+        }) 
+    })
+    roomMenu.append(imgMenu)  
+}
+
+button.addEventListener('click', ()=>{
+    // console.log("click")
+    let currLikes = parseInt(roomLikes.textContent)
+    // console.log(currLikes) 
+    let click =  ++currLikes
+    roomLikes.textContent = `${click} likes`
+    console.log(click)
+    domPatch(click)
+    console.log(button)
+}) 
+
+
+function domPatch(newLike){
+    console.log(newLike)
+
+    fetch(`http://localhost:3000/hotelRooms/${button.dataset.roomId}`, {
+        method: 'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            likes: newLike
         })
-        roomMenu.append(imgMenu)
-    }) 
-})
+    })
+    .then(res => res.json())
+    .then(room => console.log(room))
+}
+
+
 
 form.addEventListener('submit', (event) => {
-   event.preventDefault()
+event.preventDefault()
     //   console.log(event.target.review.value)  
         const newReview = document.createElement('li')
         newReview.textContent = event.target.review.value
@@ -58,17 +90,16 @@ form.addEventListener('submit', (event) => {
         event.target.reset()
 })
 
+
 const reviewRemind = document.getElementById("reviews")
-console.log(reviewRemind)
+// console.log(reviewRemind)
     reviewRemind.addEventListener('mouseover', function(){
-            alert("Don't forget to leave a review! ");})
-    
-    
-            button.innerText = "Like ❤️"
-    //button.addEventListener('click', updateLikes)
-
-//function updateLikes 
-    
+        alert("Don't forget to leave a review! ")
+    })
 
 
-   // console.log(imgMenu)
+
+
+
+
+
