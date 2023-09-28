c04586af43f52067c47fb74c638aa409734fcb4b
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-09-28 17:06:46 -0400

    form patch and project DONE

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-//URLS
 url =  "http://localhost:3000/hotelRooms"
-// const id = url/
-// console.log(id)
-//Elements
+
 const roomMenu = document.querySelector('#rooms-menu')
-// console.log(roomMenu)
 const detailImage = document.querySelector('.detail-image')
-// console.log(detailImage)
 const roomName = document.querySelector('#name')
-// console.log(roomName)
 const roomDescription = document.querySelector('#description')
-// console.log(roomDescription)
 const roomPrice = document.querySelector('#price')
-// console.log(roomPrice)
 const roomLikes = document.querySelector("#likes")
-// console.log(roomLikes)
 const reviewDisplay = document.querySelector("#reviews")
 reviewDisplay.innerHTML = ""
-//  console.log(reviewDisplay)
 const form = document.getElementById("review-form")
-// console.log(form)
 const button = document.getElementById("like-bttn")
 
-//const oldRev = document.getElementById('reviews')
-
-//const submitBttn = document.querySelector('.submit')
-//console.log(submitBttn)
-
+button.disabled = true
 
 fetch(url)
 .then(resp=> resp.json())
 .then(arrRooms => arrRooms.forEach(renderRoom))
     
 function renderRoom(roomObj){
-    // console.log(roomObj)
     const imgMenu = document.createElement("img")
     imgMenu.src = roomObj.image
     imgMenu.addEventListener("click", ()=>{
-        // console.log('click')
-        //oldRev.dataset.roomReview= roomObj.review
+        button.disabled = false
         button.dataset.roomId = roomObj.id
         form.dataset.roomId= roomObj.id
         detailImage.src = roomObj.image
@@ -49,7 +32,6 @@ function renderRoom(roomObj){
         reviewDisplay.innerHTML = ""
         roomObj.reviews.forEach(review => {
             const reviewLi = document.createElement('li') 
-            //     console.log(review)   
             reviewLi.textContent = review
             reviewDisplay.append(reviewLi)
         }) 
@@ -58,9 +40,7 @@ function renderRoom(roomObj){
 }
 
 button.addEventListener('click', ()=>{
-    // console.log("click")
-    let currLikes = parseInt(roomLikes.textContent)
-    // console.log(currLikes) 
+    let currLikes = parseInt(roomLikes.textContent) 
     let click =  ++currLikes
     roomLikes.textContent = `${click} likes`
     console.log(click)
@@ -85,55 +65,39 @@ function likePatch(newLike){
     .then(room => console.log(room))
 }
 
-
-
 form.addEventListener('submit', (event) => {
 event.preventDefault()
-    //   console.log(event.target.review.value)  
         const newReview = document.createElement('li')
         newReview.textContent = event.target.review.value
         const revId = newReview.textContent
         reviewDisplay.append(newReview) 
         event.target.reset()
         reviewPatch(revId)
-        console.log(revId)
     })
     
-
-    //fetch(url)
-   // .then(resp=> resp.json())
-    //.then(arrRooms => {
-   // })
-
-
-
-    function reviewPatch(upRev) {
+    function reviewPatch() {
+        const reviewLis = Array.from(reviewDisplay.children)
+            const revFunc= reviewLis.map((li) => {
+                return li.innerText
+            })
+        
         fetch(`http://localhost:3000/hotelRooms/${form.dataset.roomId}`, {
             method: "PATCH",
             headers:{
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                reviews: [upRev]
+                reviews: revFunc
             })
         })
         .then(res => res.json())
         .then(data => console.log(data))
     }
-  
     
-    
-
-
 const reviewRemind = document.getElementById("reviews")
-// console.log(reviewRemind)
     reviewRemind.addEventListener('mouseover', function(){
         alert("Don't forget to leave a review! ")
     })
-
-
-    //fetching the existing reviews first and then appending the new review to the existing array and then sending that as a patch request
-
 
 
 
